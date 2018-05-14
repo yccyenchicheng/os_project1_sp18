074f6435594cc8271395cc43e1fd7beeeb7416da
chihao <r05246009@ntu.edu.tw>
2018-05-14 19:41:34 +0800

    fifo with dmesg

--- src/fifo.c
+++ src/fifo.c
@@ -9,8 +9,14 @@
 #include "scheduler.h"
 #include "util.h"
 
-
 #include <assert.h>
+
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
+#include <time.h>
+#include <sys/syscall.h>
 
 #define READ_END 0
 #define WRITE_END 1
@@ -18,6 +24,12 @@
 
 
 void fifo(Process* p_arr, int N) {
+   char *tag = "[Project1]";
+    
+   struct timespec ts_start;
+   struct timespec ts_end;
+
+
    Process *rbegin, *rend, *wbegin, *wend;
    rbegin = rend = wbegin = &p_arr[0];
    wend   = (&p_arr[N]);
@@ -42,6 +54,9 @@ void fifo(Process* p_arr, int N) {
             #ifdef DEBUG
             printf("%s ready... ", wbegin->p_name);   fflush(stdout); 
             #endif
+
+            syscall(335, &ts_start);
+
             pid = fork();   wbegin->pid = pid;
             if(pid < 0) {
                perror("fork() failed...\n");
@@ -57,7 +72,10 @@ void fifo(Process* p_arr, int N) {
                   assert(sched_setscheduler(cpid, SCHED_FIFO, &sch_p) != -1);
                }       
                unit_time();  
-               
+              
+               syscall(335, &ts_end);
+               syscall(334, tag, getpid(), &ts_start, &ts_end);
+ 
                printf("%s %d\n", wbegin->p_name, getpid());   fflush(stdout);
                exit(0);
             }
